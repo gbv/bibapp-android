2ef2571845e53ca5cc1bb203fe11071a0f814117
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-07-30 19:29:30 +0200

    unused class removed

--- app/src/main/java/de/eww/bibapp/fragment/search/GVKSearchFragment.java
+++
@@ -1,9 +0,0 @@
-package de.eww.bibapp.fragment.search;
-
-import roboguice.fragment.RoboFragment;
-
-/**
- * Created by christoph on 04.11.14.
- */
-public class GVKSearchFragment extends RoboFragment {
-}
