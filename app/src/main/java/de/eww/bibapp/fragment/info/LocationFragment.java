49280407d7c803d9b372fc2e3b03470e1e7ca51b
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2017-10-13 11:07:55 +0200

    butterknife

--- app/src/main/java/de/eww/bibapp/fragment/info/LocationFragment.java
+++ app/src/main/java/de/eww/bibapp/fragment/info/LocationFragment.java
@@ -17,6 +17,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ListIterator;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.eww.bibapp.R;
 import de.eww.bibapp.model.LocationItem;
 
@@ -26,14 +28,14 @@ import de.eww.bibapp.model.LocationItem;
 public class LocationFragment extends Fragment implements
         OnMapReadyCallback {
 
-    private View mFragmentView;
-    private TextView mTitleView;
-    private TextView mAddressView;
-    private TextView mOpeningHoursView;
-    private TextView mEmailView;
-    private TextView mUrlView;
-    private TextView mPhoneView;
-    private TextView mDescriptionView;
+    @BindView(R.id.map) View fragmentView;
+    @BindView(R.id.title) TextView titleView;
+    @BindView(R.id.address) TextView addressView;
+    @BindView(R.id.opening_hours) TextView openingHoursView;
+    @BindView(R.id.email) TextView emailView;
+    @BindView(R.id.url) TextView urlView;
+    @BindView(R.id.phone) TextView phoneView;
+    @BindView(R.id.description) TextView descriptionView;
 
     private LocationItem mLocationItem = null;
 
@@ -41,15 +43,7 @@ public class LocationFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_location, container, false);
-
-        mFragmentView = view.findViewById(R.id.map);
-        mTitleView = (TextView) view.findViewById(R.id.title);
-        mAddressView = (TextView) view.findViewById(R.id.address);
-        mOpeningHoursView = (TextView) view.findViewById(R.id.opening_hours);
-        mEmailView = (TextView) view.findViewById(R.id.email);
-        mUrlView = (TextView) view.findViewById(R.id.url);
-        mPhoneView = (TextView) view.findViewById(R.id.phone);
-        mDescriptionView = (TextView) view.findViewById(R.id.description);
+        ButterKnife.bind(this, view);
 
         displayLocation();
 
@@ -62,11 +56,11 @@ public class LocationFragment extends Fragment implements
     }
 
     private void displayLocation() {
-        if (mTitleView == null || mLocationItem == null) {
+        if (this.titleView == null || mLocationItem == null) {
             return;
         }
 
-        mTitleView.setText(mLocationItem.getName());
+        this.titleView.setText(mLocationItem.getName());
 
         if (mLocationItem.hasOpeningHours()) {
             String finalOpeningHours = "";
@@ -77,64 +71,64 @@ public class LocationFragment extends Fragment implements
                 finalOpeningHours += openingHours + "\n";
             }
 
-            mOpeningHoursView.setText(finalOpeningHours);
-            mOpeningHoursView.setVisibility(View.VISIBLE);
+            this.openingHoursView.setText(finalOpeningHours);
+            this.openingHoursView.setVisibility(View.VISIBLE);
         } else {
-            mOpeningHoursView.setVisibility(View.GONE);
+            this.openingHoursView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasAddress()) {
-            mAddressView.setText(mLocationItem.getAddress());
-            mAddressView.setVisibility(View.VISIBLE);
-            Linkify.addLinks(mAddressView, Linkify.MAP_ADDRESSES);
+            this.addressView.setText(mLocationItem.getAddress());
+            this.addressView.setVisibility(View.VISIBLE);
+            Linkify.addLinks(this.addressView, Linkify.MAP_ADDRESSES);
         } else {
-            mAddressView.setVisibility(View.GONE);
+            this.addressView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasEmail()) {
-            mEmailView.setText(mLocationItem.getEmail());
-            mEmailView.setVisibility(View.VISIBLE);
-            Linkify.addLinks(mEmailView, Linkify.EMAIL_ADDRESSES);
+            this.emailView.setText(mLocationItem.getEmail());
+            this.emailView.setVisibility(View.VISIBLE);
+            Linkify.addLinks(this.emailView, Linkify.EMAIL_ADDRESSES);
         } else {
-            mEmailView.setVisibility(View.GONE);
+            this.emailView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasUrl()) {
-            mUrlView.setText(mLocationItem.getUrl());
-            mUrlView.setVisibility(View.VISIBLE);
-            Linkify.addLinks(mUrlView, Linkify.WEB_URLS);
+            this.urlView.setText(mLocationItem.getUrl());
+            this.urlView.setVisibility(View.VISIBLE);
+            Linkify.addLinks(this.urlView, Linkify.WEB_URLS);
         } else {
-            mUrlView.setVisibility(View.GONE);
+            this.urlView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasPhone()) {
-            mPhoneView.setText(mLocationItem.getPhone());
-            mPhoneView.setVisibility(View.VISIBLE);
-            Linkify.addLinks(mPhoneView, Linkify.PHONE_NUMBERS);
+            this.phoneView.setText(mLocationItem.getPhone());
+            this.phoneView.setVisibility(View.VISIBLE);
+            Linkify.addLinks(this.phoneView, Linkify.PHONE_NUMBERS);
         } else {
-            mPhoneView.setVisibility(View.GONE);
+            this.phoneView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasDescription()) {
-            mDescriptionView.setText(mLocationItem.description);
-            mDescriptionView.setVisibility(View.VISIBLE);
-            Linkify.addLinks(mDescriptionView, Linkify.WEB_URLS);
+            this.descriptionView.setText(mLocationItem.description);
+            this.descriptionView.setVisibility(View.VISIBLE);
+            Linkify.addLinks(this.descriptionView, Linkify.WEB_URLS);
         } else {
-            mDescriptionView.setVisibility(View.GONE);
+            this.descriptionView.setVisibility(View.GONE);
         }
 
         if (mLocationItem.hasPosition()) {
             SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
         } else {
-            mFragmentView.setVisibility(View.GONE);
+            this.fragmentView.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         // everything fine
-        mFragmentView.setVisibility(View.VISIBLE);
+        this.fragmentView.setVisibility(View.VISIBLE);
 
         // Move camera
         float zoomLevel = (float) (map.getMinZoomLevel() + (map.getMaxZoomLevel() - map.getMinZoomLevel()) * 0.7);
