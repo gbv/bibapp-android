e019578db2b067c26efecf1096451670ed590ceb
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-12-12 10:35:18 +0100

    schmalkalden configuration updated according to latest changes

--- app/src/schmalkalden/java/de.eww.bibapp.constants/Constants.java
+++ app/src/schmalkalden/java/de.eww.bibapp.constants/Constants.java
@@ -135,9 +135,9 @@ public final class Constants
     /**
      * The UnAPI URL for extended information
      */
-    public static String getUnApiUrl(String ppn)
+    public static String getUnApiUrl(String ppn, String format)
     {
-        return "http://unapi.gbv.de/?id=gvk:ppn:" + ppn + "&format=isbd";
+        return "http://unapi.gbv.de/?id=gvk:ppn:" + ppn + "&format=" + format;
     }
 
     /**
