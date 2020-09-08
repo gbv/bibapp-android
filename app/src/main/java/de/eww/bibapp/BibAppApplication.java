3e4f46e34ff0846d1d75c3b15e8df38e5ff95bfc
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2020-09-08 15:24:54 +0200

    min and target sdk version bumped multi-dex support

--- app/src/main/java/de/eww/bibapp/BibAppApplication.java
+++ app/src/main/java/de/eww/bibapp/BibAppApplication.java
@@ -1,41 +1,23 @@
 package de.eww.bibapp;
 
-import android.app.Application;
-import android.content.Context;
-import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
 import de.eww.bibapp.typeface.BeluginoFont;
 
 import com.mikepenz.iconics.Iconics;
-import com.mikepenz.iconics.typeface.GenericFont;
-import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by cschoenf on 07.02.17.
  */
 
-public class BibAppApplication extends Application {
+public class BibAppApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // LeakCanary
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-
         // Iconics
         Iconics.init(getApplicationContext());
         Iconics.registerFont(new BeluginoFont());
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-
-        MultiDex.install(this);
     }
 }
